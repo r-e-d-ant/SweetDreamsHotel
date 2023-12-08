c44d78fc5ea097086c33b787ba231eb5951f3d49
r-e-d-ant <mugishathierry65@gmail.com>
2023-12-09 00:14:29 +0200

    some changes

--- SweetDreamsHotel/src/main/java/SweetDreams/SweetDreamsHotel/CorsConfig.java
+++ SweetDreamsHotel/src/main/java/SweetDreams/SweetDreamsHotel/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://sweet-dreams-hotel.vercel.app/")
+                        .allowedOrigins("https://sweet-dreams-hotel.vercel.app/", "http://127.0.0.1:3000", "https://sweet-dreams-hotel.onrender.com/")
                         .allowedMethods("*") // Allow all HTTP methods
                         .allowedHeaders("*") // Allow all headers
                         .allowCredentials(true);
